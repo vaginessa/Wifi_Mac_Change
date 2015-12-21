185e109abf6696009b4202b93b8b3ea236085790
asm301 <asm301@gmail.com>
2015-12-21 01:14:38 -0600

    # changed line endings to LF

---
+++ wifimacchange/build/generated/source/buildConfig/Free/release/hom/ourhome/asm301/wifimacchange/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package hom.ourhome.asm301.wifimacchange;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "hom.ourhome.asm301.wifimacchange";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "Free";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.1.04.20151221020654";
+}
